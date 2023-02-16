3389f82a91935380b6d1b55f5e24786bacb67a5e
Laura Maarabani <lauramaarabani@gmail.com>
2023-02-16 22:09:12 +0100

    nowe moduly

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld { // [1]
+    public static void main(String[] args) {  // [2]
+        System.out.println("Hello World!"); // [3]
+        System.out.println("Laura"); // [4]
+        System.out.println(25); // [5]
+        System.out.println('M'); // [6]
+    }
+}
