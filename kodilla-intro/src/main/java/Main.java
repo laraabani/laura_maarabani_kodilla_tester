e0becaf5441da94dc667c80f0c9ae5c038aaa4fc
Laura Maarabani <lauramaarabani@gmail.com>
2023-02-16 22:52:40 +0100

    nowe cuda

---
+++ kodilla-intro/src/main/java/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        String myName = "Maciej";
+        int myAge = 22;
+        double height = 1.78;
+        char carClass = 'A';
+        boolean isGoodProgrammer = true;
+        String przywitanie = "hello from firstclass";
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(height);
+        System.out.println(carClass);
+        System.out.println(isGoodProgrammer);
+        System.out.println(przywitanie);
+    }
+}
---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String boyName = "Szymon";
+        int boyAge = 25;
+        char braSize = 'C';
+        double height = 1.92;
+        String loverName = "Laura";
+        boolean man = true;
+        System.out.println(boyName);
+        System.out.println(boyAge);
+        System.out.println(braSize);
+        System.out.println(height);
+        System.out.println(loverName);
+        System.out.println(man);
+    }
+}
